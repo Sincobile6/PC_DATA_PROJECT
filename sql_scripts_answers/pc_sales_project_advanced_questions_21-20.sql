aecaaa2a9de0527fc602d09dd69a2b1f77216f5e
Sincobile <sibisisincobile6@gmail.com>
2026-03-22 17:51:06 +0200

    corrections for intermediate and advanced sql questions on pc sales

--- sql_scripts_answers/pc_sales_project_advanced_questions_21-20.sql
+++ sql_scripts_answers/pc_sales_project_advanced_questions_21-20.sql
@@ -1,22 +1,48 @@
 -- ADVANCED QUESTIONS
 
 -- 21. Calculate profit per Shop Name.
+
 SELECT SHOP_NAME SUM(Sale_Price-Cost_Price) AS Profit_per_shop
 from pc_sales
 group by Shop_Name
 
--- 22. Calculate profit margin per sale ((Sale Price - Cost Price) / Sale Price).
+-- 22. Calculate profit margin per sale ((Sale Price - Cost Price) / Sale Price). PERCENTAGE
 
 select sales_person_name 
 sum((Sale_Price - Cost_Price) / Sale_Price) as profit_margin_per_sale
 from pc_sales
 group by sales_person_name
 
+select sales_person_name 
+sum((Sale_Price - Cost_Price) / Sale_Price) * 100.00 as profit_margin_per_sale
+from pc_sales
+group by sales_person_name
+
+SELECT
+    Pc_Make,
+    SUM(Sale_Price) AS Revenue,
+    SUM(Sale_Price) * 100.0
+    / SUM(SUM(Sale_Price))
+    OVER () AS Revenue_Percentage
+FROM PC_SALES
+GROUP BY Pc_Make
+ORDER BY Revenue
+
 -- 23. Determine which Continent has the highest total revenue.
 
 select continent sum(sale_price) as total_revenue
 from pc_sales
 group by continent
+
+SELECT DISTINCT TOP 1 CONTINENT, 
+MAX(SALE_PRICE) AS TOTAL_REVENUE
+FROM PC_SALES
+GROUP BY CONTINENT
+
+SELECT DISTINCT TOP 1 CONTINENT, 
+SUM(CAST(SALE_PRICE AS BIGINT) AS TOTAL_REVENUE
+FROM PC_SALES
+GROUP BY CONTINENT
 
 -- 24. Calculate average Sale Price per RAM size.
 
@@ -30,16 +56,34 @@ select top(10) pc_model, sale_price
 from pc_sales
 order by sale_price desc
 
+select top(1) pc_model, 
+MAX(sale_price) AS HIGHEST_SALE_PRICE
+from pc_sales
+GROUP BY PC_MODEL
+
+SELECT TOP 1 
+
+
 -- 26. Calculate the average number of days between Purchase Date and Ship Date.
 
 SELECT AVG(DATEDIFF(day, Purchase_Date, Ship_Date)) AS AvgDaysBetween
 FROM pc_sales
+
+select avg(datediff(day,try_convert(date,purchase_date),
+try_convert(date,ship_date))) as avgshipdays
+from PC_SALES
+
+SELECT AVG(DATEDIFF(day,
+TRY_CAST (Purchase_Date as datetime),
+TRY_CAST (Ship_Date as datetime))) as Avg_days
+FROM PC_SALES
 
 -- 27. Determine which Sales Person Department generates the highest revenue.
 
 select sales_person_department sum(sale_price) as highest_revenue
 from pc_sales
 group by sales_person_department
+ORDER BY TOTAL_REVENUE DESC
 
 -- 28. Calculate total revenue per Storage Capacity.
 
@@ -53,6 +97,16 @@ SELECT SALES_PERSON_NAME, Sale_Price, PC_Market_Price
 FROM pc_sales
 WHERE Sale_Price < PC_Market_Price
 
+Select Sale_Price, PC_Market_Price, PC_Make,Sales_Person_Name,PC_Model
+as sale_price_lower_than_pc_market_price
+from PC_SALES
+where Sale_Price < PC_Market_Price
+order by Sale_Price
+
+SELECT Sale_Price, PC_Market_Price
+FROM PC_SALES
+WHERE Sale_Price < PC_Market_Price
+
 -- 30. Rank Sales Person Name by Total Sales per Employee using a window function.
 SELECT sales_person_name,
     SUM(total_sales_per_employee) AS total_sales,
@@ -61,3 +115,7 @@ FROM pc_sales
 GROUP BY sales_person_name
 ORDER BY sales_rank
 
+SELECT Sales_Person_Name,
+       Total_Sales_per_Employee,
+       RANK() OVER (ORDER BY Total_Sales_Per_Employee DESC) AS Sales_Rank
+FROM PC_SALES;
--- sql_scripts_answers/pc_sales_project_intermediate_questions_11-20.sql
+++ sql_scripts_answers/pc_sales_project_intermediate_questions_11-20.sql
@@ -3,12 +3,88 @@
 -- 11. Calculate total revenue (SUM of Sale Price).
 
 select sum(Sale_price) as total_revenue
+from pc_sales;
+
+--calculate revenue by pc make
+
+select pc_make sum(convert, int(sale_price)) as revenue
+from pc_sales 
+group by pc_make;
+
+--find the number of sales per pc_make
+select pc_make
+count(sale_price) as sales_per_pc_make
 from pc_sales
+group by pc_make
+
+--find the pc above average price
+select pc_make,
+avg(sale_price) as average_price
+from pc_sales
+where avg(sale_price) >sale_price
+group by pc_make
+
+select * 
+from pc_sales
+where sale_price > (select avg(sale_price) from pc_sales) 
+
+--which brand/pc sales generates the most revenue
+
+select top 1 pc_make,
+sum(sale_price) as most_revenue
+from pc_sales
+group by pc_make
+order by most_revenue desc;
+
+--which processor type sells the most?
+
+select top 1 ram,
+count(sale_price) as sales_of_pcs
+from pc_sales
+group by ram
+order by sales_of_pcs desc
+
+--find the percentage contribution per pc make
+
+select pc_make
+sum((sale_price/196781603)*100) as percentage_contribution
+from pc_sales
+group by pc_make
+
+SELECT 
+    Pc_make,
+    SUM(Sale_Price) AS Revenue,
+    SUM(Sale_Price) * 100.0 /
+        (SELECT SUM(Sale_Price) FROM pc_sales) 
+        AS Revenue_Percentage
+FROM pc_sales
+GROUP BY pc_make
+ORDER BY Revenue DESC;
+
+
+SELECT 
+    Pc_Make,
+    SUM(Sale_Price) AS Revenue,
+    SUM(Sale_Price) * 100.0 / SUM(SUM(Sale_Price)) OVER () AS Revenue_Percentage
+FROM pc_sales
+GROUP BY Pc_Make
+ORDER BY Revenue DESC;
+
+--what is the average pc price
+
+--what is the most expensive pc sold
+
+--which brands dominate the market share?
+
+--what percentage of revenue comes from high_end machines?
+
+--which pc are priced above average market price
+
 
 -- 12. Calculate total profit (SUM of Sale Price - Cost Price).
 
 select sum(Sale_Price - Cost_Price) as Total_Profit
-from pc_sales
+from pc_sales;
 
 -- 13. Find the average Discount Amount.
 
@@ -17,12 +93,21 @@ from pc_sales
 
 -- 14. Calculate total Finance Amount issued.
 
-select sum(convert, int(finance_amount)) as Total_Finance_Amount
+select sum (cast(finance_amount) as bigint) as Total_Finance_Amount
+from pc_sales
+
+select sum( convert(bigint, finance_amount) as total_financial_amount
 from pc_sales
 
 -- 15. Find total revenue per PC Make.
 
-select pc_make sum(sale_price) as total_revenue
+select pc_make 
+sum(sale_price) as total_revenue
+from pc_sales
+group by pc_make
+
+select pc_make 
+sum(sale_price - discount_amount) as total_revenue
 from pc_sales
 group by pc_make
 
@@ -34,9 +119,9 @@ group by storage_type
 
 -- 17. Calculate total revenue per Shop Name.
 
-select shop_name sum(sale_price) as total_revenue
+select Shop_Name sum(Sale_Price) as total_revenue_per_shop
 from pc_sales
-group by shop_name
+group by Shop_Name
 
 -- 18. Calculate total revenue per Sales Person Name.
 
@@ -53,6 +138,7 @@ group by Payment_Method
 
 -- 20. Calculate total Cost of Repairs per Sales Person Department.
 
-select sales_person_department SUM(COST_OF_REPAIRS) as Total_Cost_of_repairs
+select sales_person_department 
+SUM(CAST(COST_OF_REPAIRS AS INT) as Total_Cost_of_repairs
 from pc_sales
 group by sales_person_department
